9f1ee5f048a35e451cffd86a33782cb30806066f
gidna <gidna@debian.gidna.com>
2019-09-06 10:24:56 +0200

    update all

--- calculate.c
+++ calculate.c
@@ -9,7 +9,7 @@ void calculate(struct m *matrix, int nop, int id, char *op){
         if(op[i] == 't' || op[i] == 'T')
             transpose(&matrix[0]);
         
-        else if(op[i]=='d'){
+        else if(op[i] =='d'){
             matrix[i].data[0] = determinant(matrix[i].row, &matrix[i]);
             matrix[i].row = 1;
             matrix[i].col = 1;
@@ -60,32 +60,32 @@ void calculate(struct m *matrix, int nop, int id, char *op){
             
          else if (op[i] == '+' && op[i+1] == '*'){
 			struct m tmp = multiply(&matrix[i+1], &matrix[i+2]);
-				free(matrix[i+1].data);
-				matrix[i+1] = tmp;
+			free(matrix[i+1].data);
+			matrix[i+1] = tmp;
 
-				tmp = add(&matrix[i],&matrix[i+1],+1);
-				free(matrix[i+2].data);
-	            matrix[i+2] = tmp;
+			tmp = add(&matrix[i],&matrix[i+1],+1);
+			free(matrix[i+2].data);
+	        matrix[i+2] = tmp;
          }
 
          else if (op[i] == '+' && op[i+1] == '+'){
-			 struct m tmp = add(&matrix[i],&matrix[i+1],+1);
-			 free(matrix[i+1].data);
-			 matrix[i+1] = tmp;
+			struct m tmp = add(&matrix[i],&matrix[i+1],+1);
+			free(matrix[i+1].data);
+			matrix[i+1] = tmp;
 
-			 tmp = add(&matrix[i],&matrix[i+2],+1);
-			 free(matrix[i+2].data);
-			 matrix[i+2] = tmp;
+			tmp = add(&matrix[i],&matrix[i+2],+1);
+			free(matrix[i+2].data);
+			matrix[i+2] = tmp;
          }
          
          else if (op[i] == '-' && op[i+1] == '*'){
-				struct m tmp = multiply(&matrix[i+1], &matrix[i+2]);
-				free(matrix[i+1].data);
-				matrix[i+1] = tmp;
+			struct m tmp = multiply(&matrix[i+1], &matrix[i+2]);
+			free(matrix[i+1].data);
+			matrix[i+1] = tmp;
 
-				tmp = add(&matrix[i],&matrix[i+1],-1);
-				free(matrix[i+2].data);
-	            matrix[i+2] = tmp;
+			tmp = add(&matrix[i],&matrix[i+1],-1);
+			free(matrix[i+2].data);
+	        matrix[i+2] = tmp;
          }
     }
     
