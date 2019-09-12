1df32407ab02ece5e8ad5a75ff69b0b32846dbb2
gidna <gidna@debian.gidna.com>
2019-09-12 19:53:09 +0200

    update all

--- read.c
+++ read.c
@@ -9,24 +9,36 @@ void read_file(int maxc, FILE *fp)
 {
 	struct m matrix[MAXNMATR];
     struct operator op;
-
-    size_t ncol, nrow;		    /* No of columns of a matrix */
+    size_t ncol, nrow;		    /* No of columns, rows of a matrix */
     int off = 0;
+    int check = 0;
+    double *d = NULL;
     int i;
     int n;
-    double *d = NULL;
     char buf[MAXLINE]; 			/* to store each lines of file */
-    char *p = buf;
     
     ncol = nrow = 0;
     op.id = 0;
-    op.nop = 0;
+    op.nop = 0;   
         
     for (i=0; i < MAXNOP; i++)
-            op.symbol[i] = '?';    
+        op.symbol[i] = '?';    
    
     /*Read file line by line */
-    while (fgets (buf, MAXLINE, fp)){ 
+    while (fgets (buf, MAXLINE, fp)){
+    
+        if (strcmp(buf, "\n") == 0){
+                continue;
+        }
+        
+        if (strncmp(buf, "det", 3) == 0){
+                op.symbol[op.nop++] = 'd'; 	/* determinant operation */
+                continue;
+        }
+    
+    	if (nrow == 0){      /* allocate matrix data */
+            d = matrix[op.id].data = malloc(sizeof(double) * maxc);
+        }    
         
         if (strncmp(buf, "^-1", 3) == 0){
                 op.symbol[op.nop++] = 'i'; 	/* matrix inverse operation */
@@ -37,40 +49,35 @@ void read_file(int maxc, FILE *fp)
                 op.symbol[op.nop++] = 't'; 	/* matrix transpose operation */
                 continue;
         }
-            
-        if (strncmp(buf, "det", 3) == 0){
-                op.symbol[op.nop++] = 'd'; 	/* determinant operation */
-                continue;
-        }
-        
-        if (nrow == 0){
-            /* allocate/validate max no. of matrix */
-            d = matrix[op.id].data = malloc(sizeof(double) * maxc);
-        }
-        
+      
         /*check if line contains operator */
-        if ( (!isdigit(*buf) && buf[1] =='\n'))
+        if ( *buf == '*' || *buf == '+' || *buf == '-')
         {      
             op.symbol[op.nop++] = *buf;
             matrix[op.id].col = ncol;
             matrix[op.id].row = nrow;
             nrow = ncol = 0;
             op.id++;
+            check = 0;
             continue;
         }
         
-        /* read integers in a line into d */
-        while (sscanf (p + off, "%lf%n", d, &n) == 1) {
-            d++;
-            if (nrow == 0)
-               ncol++;
-            off += n;
+   		if(check < maxc){
+		    /* read numbers in a line into d */
+		    while (sscanf (buf + off, "%lf%n", d, &n) == 1) {
+					if(check != maxc-1)
+					    d++;
+				    if (nrow == 0)
+				       ncol++;
+				    off += n;
+				    check++;
+		   }
         }
         
         nrow++;
         off = 0;
-    }				/* end of while fgets cycle */
-
+    }			/* end of while fgets cycle */
+    
 	fclose(fp);
        
     /* Assign last matrix No of columns and rows */
@@ -82,6 +89,6 @@ void read_file(int maxc, FILE *fp)
         printf("%c\n", op.symbol[0]);
         exit(1);
     }
-    
+ 
     display_data(matrix, &op);
 }
