221e945fa9cb7fcd8bbac2a928cc4d36daa00328
gidna <gidna@debian.gidna.com>
2019-09-16 11:43:16 +0200

    update all

--- calculate.c
+++ calculate.c
@@ -61,56 +61,25 @@ void calculate(struct m *matrix, struct operator *op){
              break;
         }     
          
-        if(op->symbol[i] == '*' && op->symbol[i+1] == 'd'){		/* Matrix1 * det(Maitrix2) or number * det(Maitrix2)*/
-        	  
-           	  if (matrix[i].row == 1 && matrix[i].col == 1){     
-           	        	  
+        if(op->symbol[i] == '*' && op->symbol[i+1] == 'd' && matrix[i].row == 1 && matrix[i].col == 1){		/* Matrix1 * det(Maitrix2) or number * det(Maitrix2)*/
+
                  tmp = scalar_product(matrix[i].data[0], &matrix[i+1]);  /* Multiplication of Scalar per matrix */
                  copy_matrix(&matrix[i+1], &tmp);
 
                  if(op->symbol[i+2] == '?')
 	              	  break;
               	 
-              	 else if(op->symbol[i+2] == '+'){
-              	  	tmp = add(&matrix[i+1], &matrix[i+2], op->symbol[i+2]);
-	       	 	  	copy_matrix(&matrix[i+2], &tmp);
-	       	 	 }
+    	         else if(op->symbol[i+2] == '*'){
+				        tmp = scalar_product(matrix[i+1].data[0], &matrix[i+2]);  /* Multiplication of Scalar per matrix */
+				        copy_matrix(&matrix[i+2], &tmp);
+		              	break;
+	       		 }
 	       		 
-	       	  	 else if(op->symbol[i+2] == '-'){
-             	 	tmp = add(&matrix[i+1], &matrix[i+2], op->symbol[i+2]);
-	       	 	 	copy_matrix(&matrix[i+2], &tmp);
-	       	  	 }
-	       	
-	             else if(op->symbol[i+2] == '*'){
-             		tmp = multiply(&matrix[i+1], &matrix[i+2]);
-             	    copy_matrix(&matrix[i+2], &tmp);
-	       		}
-             }
-             
-          else{
-          		 tmp = multiply(&matrix[i], &matrix[i+1]);  /* Multiplication of Scalar per matrix */
-                 copy_matrix(&matrix[i+1], &tmp);
-
-                 if(op->symbol[i+2] == '?')
-	              	  break;
-              	 
-              	 else if(op->symbol[i+2] == '+'){
-              	  	tmp = add(&matrix[i+1], &matrix[i+2], op->symbol[i+2]);
-	       	 	  	copy_matrix(&matrix[i+2], &tmp);
-	       	 	 }
-	       		 
-	       	  	 else if(op->symbol[i+2] == '-'){
-             	 	tmp = add(&matrix[i+1], &matrix[i+2], op->symbol[i+2]);
-	       	 	 	copy_matrix(&matrix[i+2], &tmp);
-	       	  	 }
-	       	
-	             else if(op->symbol[i+2] == '*'){
-             		tmp = multiply(&matrix[i+1], &matrix[i+2]);
-             	    copy_matrix(&matrix[i+2], &tmp);
-	       		}
-             }
-            
-  		 	continue;
+	       		 else{
+	       		 	printf("Matrixlab error: Wrong dimension\n");
+           			break;
+           		}
+  		 		continue;
   		 }
   		 
          else if(op->symbol[i] == '*' && (op->symbol[i+1] == '+' || op->symbol[i+1] == '-')){   /* matrix * matrix -/+ matrix */
@@ -119,31 +88,31 @@ void calculate(struct m *matrix, struct operator *op){
                  tmp = scalar_product(matrix[i].data[0], &matrix[i+1]);  /* Multiplication of Scalar per matrix */
                  copy_matrix(&matrix[i+1], &tmp);
                  tmp = add(&matrix[i+1], &matrix[i+2], op->symbol[i+1]);
-			 	 copy_matrix(&matrix[i+2], &tmp);                 
-             }
+			 	 copy_matrix(&matrix[i+2], &tmp);
+              }             
              
-             else{
+              else{
              	 tmp = multiply(&matrix[i], &matrix[i+1]);
-             	 copy_matrix(&matrix[i+1], &tmp);             	 
+             	 copy_matrix(&matrix[i+1], &tmp);
              	 tmp = add(&matrix[i+1], &matrix[i+2], op->symbol[i+1]);
 			 	 copy_matrix(&matrix[i+2], &tmp);
-             }
+              }
              
-             continue;
+              continue;
         }
                
           else if(op->symbol[i] == '*' && op->symbol[i+1] == '*'){
           
              if (matrix[i].row == 1 && matrix[i].col == 1){
                  tmp = scalar_product(matrix[i].data[0], &matrix[i+1]);    /* Multiplication of Scalar per matrix*/
-                 copy_matrix(&matrix[i+1], &tmp);                 
+                 copy_matrix(&matrix[i+1], &tmp);
                  tmp = multiply(&matrix[i+1], &matrix[i+2]);
 			 	 copy_matrix(&matrix[i+2], &tmp);
              }
              
              else{
              	 tmp = multiply(&matrix[i], &matrix[i+1]);
-             	 copy_matrix(&matrix[i+1], &tmp);             	 
+             	 copy_matrix(&matrix[i+1], &tmp);
              	 tmp = multiply(&matrix[i+1], &matrix[i+2]);
 			 	 copy_matrix(&matrix[i+2], &tmp);
              }
@@ -151,15 +120,15 @@ void calculate(struct m *matrix, struct operator *op){
              continue;
         }
             
-         else if ( (op->symbol[i] == '+' || op->symbol[i] == '-') && op->symbol[i+1] == '*'){
+         else if ((op->symbol[i] == '+' || op->symbol[i] == '-') && op->symbol[i+1] == '*'){
 			tmp = multiply(&matrix[i+1], &matrix[i+2]);
 			copy_matrix(&matrix[i+1], &tmp);
-			tmp = add(&matrix[i],&matrix[i+1], op->symbol[i]);
+			tmp = add(&matrix[i], &matrix[i+1], op->symbol[i]);
 			copy_matrix(&matrix[i+2], &tmp);
 	        continue;
          }
 
-         else if ( (op->symbol[i] == '+' || op->symbol[i] == '-') && (op->symbol[i+1] == '+' || op->symbol[i+1] == '-')){
+         else if ((op->symbol[i] == '+' || op->symbol[i] == '-') && (op->symbol[i+1] == '+' || op->symbol[i+1] == '-')){
 			 tmp = add(&matrix[i],&matrix[i+1], op->symbol[i+1]);
 			 copy_matrix(&matrix[i+1], &tmp);
 			 tmp = add(&matrix[i+1],&matrix[i+2], op->symbol[i+1]);
